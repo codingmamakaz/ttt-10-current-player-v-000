2ec02eaca4b8fdf82782e40b48de1d6cbd3cb5bc
Kazumi Karbowski <kazkazinus@gmail.com>
2018-03-17 20:08:58 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,7 +2,7 @@ def turn_count(board)
   counter = 0
   board.each do |numberofturns|
     puts "#{counter}"
-    if numberofturns !== ("X"||"O")
+    if numberofturns == (" "||"")
     counter += 1
   else
     
