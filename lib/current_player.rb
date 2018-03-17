0e4fbb91a11b526081ac4ae8a99352a5f82f9f32
Kazumi Karbowski <kazkazinus@gmail.com>
2018-03-17 19:22:46 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  counter = 0
+  board.each do|number|
+    puts counter
+    counter +=
+  end
+
+
+end
+
+# counter = 0
+# board.each do|turn|
+#   if turn > 0 && turn < 9
+#     counter +=
