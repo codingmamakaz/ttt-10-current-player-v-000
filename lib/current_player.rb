a95a6f3124b7c489be7be900b2638b48834e4734
Kazumi Karbowski <kazkazinus@gmail.com>
2018-03-17 21:01:08 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,10 +1,10 @@
 def turn_count(board)
   counter = 0
-  board.each do |turns|
-    if (turns == "X")||(turns == "O")
+  board.each do |turn|
+    if (turn == "X")||(turn == "O")
     counter += 1
     else
     end
   end
-  puts counter
+  counter
 end
